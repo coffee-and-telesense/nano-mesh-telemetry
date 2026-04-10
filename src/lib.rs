4ec350077fb8cda54b63872db23d8c05695bd3d8
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2026-04-10 12:23:18 -0700

    just messing around

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,19 @@
 //! unified telemetry type structure (e.g. CO2, Temperature, and other telemetry types) across
 //! sensors and mesh members. The library provides a format for serialization/deserialization to and
 //! from the embedded-nano-mesh packet type
+
+use embedded_nano_mesh::PacketDataBytes;
+
+/// A `Sensor` connected to a node
+pub trait Sensor {
+    /// Setup a sensor
+    fn setup(&mut self);
+}
+
+/// The `Telemetry` a `Sensor` returns
+pub trait Telemetry {
+    /// Serialize the telemetry into `PacketDataBytes`
+    fn serialize(&self) -> PacketDataBytes;
+    /// Deserialize the telemetry from `PacketDataBytes`
+    fn deserialize(data: PacketDataBytes) -> Self;
+}
